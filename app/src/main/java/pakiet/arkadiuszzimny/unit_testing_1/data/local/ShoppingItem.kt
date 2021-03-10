c40b391af7b0e5544366b490d157c6fee31f77ff
Colderz <arekzim98@gmail.com>
2021-03-10 22:31:53 +0100

    Create local database and template for tests

---
+++ app/src/main/java/pakiet/arkadiuszzimny/unit_testing_1/data/local/ShoppingItem.kt
@@ -0,0 +1,14 @@
+package pakiet.arkadiuszzimny.unit_testing_1.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "shopping_items")
+data class ShoppingItem(
+    var name: String,
+    var amount: Int,
+    var price: Float,
+    var imageUrl: String,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int? = null
+)
---
+++ app/src/main/java/pakiet/arkadiuszzimny/unit_testing_1/data/local/ShoppingItemDatabase.kt
@@ -0,0 +1,14 @@
+package pakiet.arkadiuszzimny.unit_testing_1.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [ShoppingItem::class],
+    version = 1
+)
+abstract class ShoppingItemDatabase : RoomDatabase() {
+
+    abstract fun shoppingDao(): ShoppingDao
+
+}
